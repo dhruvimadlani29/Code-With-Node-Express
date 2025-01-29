34b73b572083eef3b9884b798493785d86be300a
dhruvimadlani29 <dhruvimadlani2902@gmail.com>
2025-01-28 20:24:40 -0500

    Create get API using express

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+app.get('/', (req, res) => res.send('Hello World'));
+app.get('/form', (req, res) => {
+  return res.send(`
+      <div class="container">
+        <h1>URL Shortener</h1>
+        <form id="shorten-form">
+          <div>
+            <label for="url">Enter URL</label>
+            <input type="url" id="url" name="url" required />
+          </div>
+          <div>
+            <label for="shortCode">Enter Short Code</label>
+            <input type="text" id="shortCode" name="shortCode" required />
+          </div>
+          <button type="submit">Shorten</button>
+        </form>
+        <h2>Shortened URL</h2>
+        <ul id="shortened-urls"></ul>
+      </div>
+    `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port : ${PORT}`);
+});
